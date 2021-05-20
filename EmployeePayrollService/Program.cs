74fe8795a8e752dc38d491bc70253db3908ce663
vundelayamini <vundelayamini@gmail.com>
2021-05-20 19:48:29 +0530

    [yamini]Add: Welcome message to Employee payroll service

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Payroll Service");
+        }
+    }
+
+  
+        
+    
+}
